a3f928c464e72914f3f263b8d0e3d4762b799ceb
LENOVO <LENOVO@LENOVO-PC>
2017-05-15 10:36:35 -0400

    modificaciones en los archivos

--- rrhh/usuarios/form.php
+++ rrhh/usuarios/form.php
@@ -1,7 +1,7 @@
 <?php include('../../view_header_app.php')?>
 <?php include('../menu.php')?>
 
-<h1 class="text-center">Procesos</h1>
+<h1 class="text-center">Usuarios</h1>
 <form action="index.php" method="post" enctype="multipart/form-data">
     <input type="text" name="action" value="<?php if(isset($values['action']) and $values['action']!='') echo $values['action'];?>">
     
